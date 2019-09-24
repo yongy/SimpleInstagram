93cea2900679c699b902c5a11b3456be9c874730
yongy <yuyong05@gmail.com>
2019-09-23 23:29:57 -0700

    Instagram - support CRUD

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views
+from Insta.views import (HellowWorld, PostsView, PostDetailView, PostCreateView, 
+                         PostDeleteView, PostUpdateView)
 
 urlpatterns = [
-    path('', views.HellowWorld.as_view(), name = 'helloword')
+    path('', HellowWorld.as_view(), name = 'helloworld'),
+    path('posts/', PostsView.as_view(), name = 'posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'create_post'),    
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name = 'update_post'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name = 'delete_post')
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -1,6 +1,38 @@
-from django.views.generic import TemplateView
+
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
+from .models import Post
 
 # Create your views here.
 
 class HellowWorld(TemplateView):
     template_name = 'home.html'
+
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
