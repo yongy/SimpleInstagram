85d36a6fe47b1799f6721dabd20a7df3073efcbb
yongy <yuyong05@gmail.com>
2019-09-29 15:59:03 -0700

    SimpleInstagram : prep for deployment

--- InstaDemo/settings.py
+++ InstaDemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y^^ph-as&p#4b)6e^9k-pfk4cjhj7iqh4$r7cxs1fuz+ys5r+n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'Insta'
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'InstaDemo.urls'
